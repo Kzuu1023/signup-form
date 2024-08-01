6e780ef9a5f2d7462c7f47d548e69c3c5be1883d
kzuu1023 <161953460+Kzuu1023@users.noreply.github.com>
2024-08-01 11:54:31 -0700

    refactor: use checkValidity and add border color each condition

--- script.js
+++ script.js
@@ -5,34 +5,86 @@ const inputPassword = document.getElementById("password");
 const confirmPassword = document.getElementById("confirm_password");
 const inputPhoneNumber = document.getElementById("phone_number");
 const submitBtn = document.getElementById("submit-btn");
-
+const errorMsg = document.querySelector(".error-msg");
+const form = document.getElementById("form");
 const inputs = [
     inputFirstName,
     inputLastName,
     inputEmail,
+    inputPhoneNumber,
     inputPassword,
     confirmPassword,
-    inputPhoneNumber,
 ];
 
+let isValid = true;
 submitBtn.addEventListener("click", function () {
+    inputs.forEach((input) => {
+        input.nextElementSibling.textContent = "";
+        console.log(typeof input.checkValidity());
+        if (input.checkValidity()) {
+            input.parentElement.classList.add("valid");
+            input.parentElement.classList.remove("invalid");
+        } else {
+            input.parentElement.classList.add("invalid");
+            input.parentElement.classList.remove("valid");
+            isValid = false;
+        }
+    });
+
     if (!inputFirstName.value) {
-        console.log("First name cannot be empty");
+        isValid = false;
+
+        inputFirstName.nextElementSibling.textContent =
+            "First name cannot be empty";
+        inputFirstName.style.borderColor = "var(--error)";
+    } else {
+        inputFirstName.style.borderColor = "var(--valid)";
     }
 
     if (!inputLastName.value) {
-        console.log("Last name cannot be empty");
+        isValid = false;
+        inputLastName.nextElementSibling.textContent =
+            "Last name cannot be empty";
+        inputLastName.style.borderColor = "var(--error)";
+    } else {
+        inputLastName.style.borderColor = "var(--valid)";
     }
 
     if (!inputEmail.value) {
-        console.log("Email cannot be empty");
+        inputEmail.nextElementSibling.textContent = "Email cannot be empty";
+        inputEmail.style.borderColor = "var(--error)";
+    } else {
+        inputEmail.style.borderColor = "var(--valid)";
     }
 
     if (!inputPassword.value) {
-        console.log("Password cannot be empty");
+        inputPassword.nextElementSibling.textContent =
+            "Password cannot be empty";
+        inputPassword.style.borderColor = "var(--error)";
+    } else {
+        inputPassword.style.borderColor = "var(--valid)";
     }
 
     if (!inputPhoneNumber.value) {
-        console.log("Phone Number cannot be empty");
+        inputPhoneNumber.nextElementSibling.textContent =
+            "Phone Number cannot be empty";
+        inputPhoneNumber.style.borderColor = "var(--error)";
+    } else {
+        inputPhoneNumber.style.borderColor = "var(--valid)";
     }
+
+    if (
+        confirmPassword.value !== inputPassword.value ||
+        !confirmPassword.value
+    ) {
+        confirmPassword.nextElementSibling.textContent =
+            "Confirm Password does not match";
+        confirmPassword.style.borderColor = "var(--error)";
+    } else {
+        confirmPassword.style.borderColor = "var(--valid)";
+    }
+});
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
 });
